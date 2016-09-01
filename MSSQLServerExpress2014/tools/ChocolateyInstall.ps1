b78102d46ccf6c434472f8a02f9239110e97dd11
Boyan Kostadinov <boyank@gmail.com>
2016-09-01 19:53:09 -0400

    Added chocolatey install script
    Added NuGet spec and tools for MSSSQLServer2014Express package

---
+++ MSSQLServerExpress2014/tools/ChocolateyInstall.ps1
@@ -0,0 +1,35 @@
+# https://github.com/riezebosch/BoxstarterPackages
+
+$packageName = "MSSQLServer2014Express"
+$chocolateyTempDir = Join-Path (Get-Item $env:TEMP).FullName "chocolatey"
+$tempDir = Join-Path $chocolateyTempDir $packageName
+$fileFullPath = "$tempDir\SQLEXPR.exe"
+$extractPath = "$tempDir\SQLEXPR"
+$setupPath = "$extractPath\setup.exe"
+$url = "https://download.microsoft.com/download/1/5/6/156992E6-F7C7-4E55-833D-249BD2348138/ENU/x86/SQLEXPR_x86_ENU.exe"
+$url64 = "https://download.microsoft.com/download/1/5/6/156992E6-F7C7-4E55-833D-249BD2348138/ENU/x64/SQLEXPR_x64_ENU.exe"
+$checksum = '33C0112905B62B6BAD883112C2F49B50AA12C679'
+$checksum64 = '0C90C147A1C2A550165C9301AE7A6C604E318E51'
+
+$sqlServerConfigurationUrl = "https://raw.githubusercontent.com/Boyan-Kostadinov/BoxStarter/master/SQLServer-Configuration.ini"
+$sqlServerConfigurationFile = "SQLServer-Configuration.ini"
+$sqlServerConfigurationFilePath = Join-Path (Join-Path (Get-Item $env:TEMP).FullName "chocolatey") $sqlServerConfigurationFile
+$silentArgs = "/IAcceptSQLServerLicenseTerms ConfigurationFile=""$($sqlServerConfigurationFilePath)"" /SAPWD=""SetYourOwn"""
+
+# SQL Server Parameters - https://msdn.microsoft.com/en-us/library/ms144259.aspx
+# How to Create the Configuration INI - https://msdn.microsoft.com/en-us/library/dd239405.aspx
+# Get the Configuration file
+Get-ChocolateyWebFile $sqlServerConfigurationFile $sqlServerConfigurationFilePath $sqlServerConfigurationUrl
+
+# Create the directory if it doesn't exist
+[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null
+Get-ChocolateyWebFile $packageName $fileFullPath $url $url64 -Checksum $checksum -Checksum64 $checksum64 -ChecksumType "sha1"
+
+Write-Host "Extracting SQL Server Express Package..."
+Start-Process "$fileFullPath" "/Q /x:`"$extractPath`"" -Wait
+
+Write-Host "Installing SQL Server Express..."
+Install-ChocolateyInstallPackage "$packageName" "EXE" "$silentArgs" "$setupPath" -validExitCodes @(0, 3010)
+
+Write-Host "Removing SQL Server Express Extracted Files..."
+rm -r "$extractPath"
