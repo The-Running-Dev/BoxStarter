6d4d0e75eff0bf056559f33bb3187944ecc6951e
Boyan Kostadinov <boyan@ensembleVideo.com>
2016-09-01 17:44:32 -0400

    Added cuistom SQL Server install script
    Based on the Chocolatey package found here: https://chocolatey.org/packages/MsSqlServer2014Express

---
+++ SQLServer-Install.ps1
@@ -0,0 +1,30 @@
+$packageName = "MsSqlServer2014Express"
+$chocolateyTempDir = Join-Path (Get-Item $env:TEMP).FullName "chocolatey"
+$tempDir = Join-Path $chocolateyTempDir $packageName
+$fileFullPath = "$tempDir\SQLEXPR.exe"
+$extractPath = "$tempDir\SQLEXPR"
+$setupPath = "$extractPath\setup.exe"
+$url = "https://download.microsoft.com/download/1/5/6/156992E6-F7C7-4E55-833D-249BD2348138/ENU/x86/SQLEXPR_x86_ENU.exe"
+$url64 = "https://download.microsoft.com/download/1/5/6/156992E6-F7C7-4E55-833D-249BD2348138/ENU/x64/SQLEXPR_x64_ENU.exe"
+$sqlServerConfigurationUrl = "https://raw.githubusercontent.com/Boyan-Kostadinov/BoxStarter/master/SQLServer-Configuration.ini"
+$sqlServerConfigurationFile = "SQLServer-Configuration.ini"
+$sqlServerConfigurationFilePath = Join-Path (Join-Path (Get-Item $env:TEMP).FullName "chocolatey") $sqlServerConfigurationFile
+$silentArgs = "/IAcceptSQLServerLicenseTerms ConfigurationFile=""$($sqlServerConfigurationFilePath)"" /SAPWD=""SetYourOwn"""
+
+# SQL Server Parameters - https://msdn.microsoft.com/en-us/library/ms144259.aspx
+# How to Create the Configuration INI - https://msdn.microsoft.com/en-us/library/dd239405.aspx
+# Get the Configuration file
+Invoke-WebRequest -Uri $sqlServerConfigurationUrl -OutFile $sqlServerConfigurationFilePath
+
+# Create the directory if it doesn't exist
+[System.IO.Directory]::CreateDirectory($tempDir) | Out-Null
+Get-ChocolateyWebFile $packageName $fileFullPath $url $url64
+
+Write-Host "Extracting SQL Server Express Package..."
+Start-Process "$fileFullPath" "/Q /x:`"$extractPath`"" -Wait
+
+Write-Host "Installing SQL Server Express..."
+Install-ChocolateyInstallPackage "$packageName" "EXE" "$silentArgs" "$setupPath" -validExitCodes @(0, 3010)
+
+Write-Host "Removing SQL Server Express Extracted Files..."
+rm -r "$extractPath"
