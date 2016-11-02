b4d42fc1d15e6499996931483154d94110089d3f
Boyan Kostadinov <boyank@gmail.com>
2016-11-02 11:23:46 -0400

    Created IIS package.

---
+++ Chocolatey-Packages/WinIIS/tools/ChocolateyInstall.ps1
@@ -0,0 +1,5 @@
+Import-Module (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'Helpers.psm1')
+
+Import-Module (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'Helpers.psm1')
+
+InstallWindowsFeatures
---
+++ Chocolatey-Packages/WinIIS/tools/ChocolateyUninstall.ps1
@@ -0,0 +1,1 @@
+Import-Module (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'Helpers.psm1')
