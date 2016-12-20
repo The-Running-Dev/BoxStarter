033e8b30b5f8b5212d7e440a169c21181eda4593
Boyan Kostadinov <boyank@gmail.com>
2016-12-20 18:34:30 -0500

    Renamed LastPass to LastPass.Applications

--- Boyan-Choco.extension/extensions/ChocoHelpers.psm1
+++ Boyan-Choco.extension/extensions/ChocoHelpers.psm1
@@ -3,7 +3,7 @@ function Install-Applications([string] $file)
     Write-Host "Installing Applications from $file"
 
     if ($env:packagesSource) {
-        $packagesSource = "-s ""$env:packagesSource;chocolatey"""
+        $packagesSource = "-s ""$env:packagesSource;Chocolatey"""
     } 
 
     Invoke-Commands $file "choco install ##token## -r --execution-timeout 14400 -y $packagesSource"
@@ -14,7 +14,7 @@ function UnInstall-Applications([string] $file)
     Write-Host "Uninstalling Applications from $file"
 
     if ($env:packagesSource) {
-        $packagesSource = "-s ""$env:packagesSource;chocolatey"""
+        $packagesSource = "-s ""$env:packagesSource;Chocolatey"""
     } 
 
     Invoke-Commands $file "choco uninstall ##token## -r --execution-timeout 14400 -y $packagesSource"
