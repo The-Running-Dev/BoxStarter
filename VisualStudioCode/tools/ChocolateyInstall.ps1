aa7bc1bdcc087a67f5df97d77f21ec7ef41cd2d8
Boyan Kostadinov <bkostadinov@gocontec.com>
2017-02-13 16:09:21 -0500

    Updated VSCode to 1.9.1

--- VisualStudioCode/tools/ChocolateyInstall.ps1
+++ VisualStudioCode/tools/ChocolateyInstall.ps1
@@ -1,6 +1,5 @@
 $ErrorActionPreference = 'Stop';
 
-# Default values
 $createDesktopIcon = $false
 $createQuickLaunchIcon = $true
 $addContextMenuFiles = $true
@@ -9,70 +8,60 @@ $addToPath = $true
 
 $parameters = Get-Parameters $env:chocolateyPackageParameters
 
-if ($parameters.ContainsKey("nodesktopicon"))
-{
+if ($parameters.ContainsKey("nodesktopicon")) {
     $createDesktopIcon = $false
 }
 
-if ($parameters.ContainsKey("noquicklaunchicon"))
-{
+if ($parameters.ContainsKey("noquicklaunchicon")) {
     $createQuickLaunchIcon = $false
 }
 
-if ($parameters.ContainsKey("nocontextmenufiles"))
-{
+if ($parameters.ContainsKey("nocontextmenufiles")) {
     $addContextMenuFiles = $false
 }
 
-if ($parameters.ContainsKey("nocontextmenufolders"))
-{
+if ($parameters.ContainsKey("nocontextmenufolders")) {
     $addContextMenuFolders = $false
 }
 
-if ($parameters.ContainsKey("dontaddtopath"))
-{
+if ($parameters.ContainsKey("dontaddtopath")) {
     $addToPath = $false
 }
 
 $mergeTasks = "!runCode"
 
-if ($createDesktopIcon)
-{
+if ($createDesktopIcon) {
     $mergeTasks = $mergeTasks + ",desktopicon"
 }
 
-if ($createQuickLaunchIcon)
-{
+if ($createQuickLaunchIcon) {
     $mergeTasks = $mergeTasks + ",quicklaunchicon"
 }
 
-if ($addContextMenuFiles)
-{
+if ($addContextMenuFiles) {
     $mergeTasks = $mergeTasks + ",addcontextmenufiles"
 }
 
-if ($addContextMenuFolders)
-{
+if ($addContextMenuFolders) {
     $mergeTasks = $mergeTasks + ",addcontextmenufolders"
 }
 
-if ($addToPath)
-{
+if ($addToPath) {
     $mergeTasks = $mergeTasks + ",addtopath"
 }
 
 $script           = $MyInvocation.MyCommand.Definition
 $arguments          = @{
-  packageName     = $packageName
-  unzipLocation   = (Get-CurrentDirectory $script)
-  fileType        = 'exe'
-  file            = Join-Path (Get-ParentDirectory $script) 'VSCodeSetup-1.9.0.exe'
-  url             = 'https://go.microsoft.com/fwlink/?LinkID=623230'
-  softwareName    = 'VisualStudioCode*'
-  checksum        = 'BC9BA15E179CC1CE3E3B80B16FB0F778B6BA71020BE5C6E6B9B73ADB0F7A63E9'
-  checksumType    = 'sha256'
-  silentArgs      = "/verysilent /suppressmsgboxes /mergetasks=$mergeTasks /log=""$env:temp\vscode.log"""
-  validExitCodes  = @(0, 3010, 1641)
+    packageName     = $packageName
+    unzipLocation   = (Get-CurrentDirectory $script)
+    fileType        = 'exe'
+    file            = Join-Path (Get-ParentDirectory $script) 'VSCodeSetup-1.9.0.exe'
+    url             = 'https://go.microsoft.com/fwlink/?LinkID=623230'
+    softwareName    = 'VisualStudioCode*'
+    checksum        = 'CC463AD26D0816A9422F254FF33FB0F394CEB2C16AC4A42B8E1DA0C62A99173B'
+    checksumType    = 'sha256'
+    silentArgs      = "/verysilent /suppressmsgboxes /mergetasks=$mergeTasks /log=""$env:temp\vscode.log"""
+    validExitCodes  = @(0, 3010, 1641)
 }
 
 Install-LocalOrRemote $arguments
