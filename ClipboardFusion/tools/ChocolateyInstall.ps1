d2694c31cca3a48eda3657b33bcdeffd5e28d235
Boyan Kostadinov <bkostadinov@gocontec.com>
2017-03-24 12:18:16 -0400

    Updated ClipboardFusion to 5.4

--- ClipboardFusion/tools/ChocolateyInstall.ps1
+++ ClipboardFusion/tools/ChocolateyInstall.ps1
@@ -1,15 +1,17 @@
-$script           = $MyInvocation.MyCommand.Definition
-$arguments        = @{
-  packageName     = 'ClipboardFusion'
-  unzipLocation   = (Get-CurrentDirectory $script)
-  fileType        = 'exe'
-  file            = Join-Path (Get-ParentDirectory $script) 'ClipboardFusionSetup-4.2.exe'
-  url             = 'https://www.binaryfortress.com/Data/Download/?package=clipboardfusion&log=104'
-  softwareName    = 'ClipboardFusion*'
-  checksum        = 'EF1ABF166F7D76F73B2E245207B0D26BDB87840CB6835BE705161610DCA6498E'
-  checksumType    = 'sha256'
-  silentArgs      = '/VERYSILENT /LAUNCHAFTER=0'
-  validExitCodes  = @(0, 3010, 1641)
+$installer          = 'ClipboardFusionSetup-5.0-Beta4.exe'
+$url                = 'https://www.binaryfortress.com/Data/Download/?package=clipboardfusion&beta=1&log=104'
+$checksum           = '732A157BD32C1B7679103FD59CC07D60E251354ED000E082113D60BEEE8C1F56'
+$arguments          = @{
+    packageName     = $env:ChocolateyPackageName
+    softwareName    = $evn:ChocolateyPackageTitle
+    unzipLocation   = $env:ChocolateyPackageFolder
+    file            = Join-Path $env:ChocolateyPackageFolder $installer
+    url             = $url
+    checksum        = $checksum
+    fileType        = 'exe'
+    checksumType    = 'sha256'
+    silentArgs      = '/VERYSILENT /LAUNCHAFTER=0'
+    validExitCodes  = @(0, 1641, 3010)
 }
 
 Install-LocalOrRemote $arguments
