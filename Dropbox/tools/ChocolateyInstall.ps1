797d6e1cfbd9bb8f6fd1137f5b80838de1d1ac48
Boyan Kostadinov <bkostadinov@gocontec.com>
2017-03-30 18:36:52 -0400

    Fixed type iin package title in Dropbox install script

--- Dropbox/tools/ChocolateyInstall.ps1
+++ Dropbox/tools/ChocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$installer          = 'Dropbox%2022.4.24%20Offline%20Installer.exe'
 $url                = 'https://clientupdates.dropboxstatic.com/client/Dropbox%2022.4.24%20Offline%20Installer.exe'
 $checksum           = '37d27d3ebd0426be061495e9efd4d67f7e3dd3b582f89fea6a31b45b3fb9beb0'
-$packageArgs        = @{
+$arguments        	= @{
     packageName     = $env:ChocolateyPackageName
-    softwareName    = $evn:ChocolateyPackageTitle
+    softwareName    = $env:ChocolateyPackageTitle
     unzipLocation   = $env:ChocolateyPackageFolder
     url             = $url
     checksum        = $checksum
@@ -13,8 +13,8 @@ $packageArgs        = @{
     validExitCodes = @(0, 1641, 3010)
 }
 
-Install-CustomPackage @packageArgs
+Install-CustomPackage $arguments
 
-if (Get-Process -Name Dropbox) {
+if (Get-Process -Name Dropbox -ErrorAction SilentlyContinue) {
     Stop-Process -processname Dropbox
 }
