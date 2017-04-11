e7b49ac84185febadee311dc24f84e56398005f9
The Running Dev <therunningdev@gmail.com>
2017-04-10 19:01:11 -0500

    Cleanup of build  scripts and external packages

--- push.ps1
+++
@@ -1,7 +0,0 @@
-param (
-    [string] $searchTerm,
-    [ValidateSet('Remote', 'Local')][String] $sourceType = 'local'
-)
-Import-Module (Join-Path $PSScriptRoot 'build-helpers.psm1') -Force
-
-Invoke-Push $PSScriptRoot $searchTerm $sourceType
