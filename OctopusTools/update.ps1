e489a7bc94351456f6471ec6ffc2683c80c29de0
The Running Dev <therunningdev@gmail.com>
2017-04-12 09:38:59 -0400

    Added OctopusTools package and update script

---
+++ OctopusTools/update.ps1
@@ -0,0 +1,14 @@
+. (Join-Path $PSScriptRoot '..\Build\update.common.ps1')
+
+function global:au_GetLatest {
+    $releaseUrl = 'https://octopus.com/downloads'
+    $downloadLinkRegEx = 'OctopusTools\.([0-9\.]+)\.zip'
+
+    $downloadPage = Invoke-WebRequest -UseBasicParsing -Uri $releaseUrl
+    $url = $downloadPage.Links | Where-Object href -match $downloadLinkRegEx | Select-Object -First 1 -Expand href
+    $version = [regex]::match($url, $downloadLinkRegEx).Groups[1].Value
+
+    return @{ Url32 = $url; Version = $version }
+}
+
+Update-Package -ChecksumFor none -NoCheckChocoVersion
