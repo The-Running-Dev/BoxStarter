229446e1b7309614da957d54f223c44a34736323
The Running Dev <therunningdev@gmail.com>
2017-04-18 17:00:38 -0400

    New Recuva package

---
+++ Recuva/tools/Chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$arguments          = @{
+    file            = 'rcsetup153.exe'
+    url             = 'http://download.piriform.com/rcsetup153.exe'
+    checksum        = '75155568D64E958D8003F9FBB36839FC9A53BFAB3B51A8A1106A78E5BE98B2E9'
+    silentArgs      = '/S'
+}
+
+Install-Package $arguments
