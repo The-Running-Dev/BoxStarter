1b86e3c78f5fa70aae942cfb910844e88fa9d152
The Running Dev <therunningdev@gmail.com>
2017-05-07 22:54:56 -0400

    Renamed VMWare-Workstation

---
+++ VMWare-Workstation/tools/ChocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$arguments          = @{
+    url             = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-12.5.5-5234757.exe'
+    checksum        = 'EF471E8080A7B7B9B2947A8033C32D6F346DC5A21F61022BA49B1DF77BEDDB55'
+    silentArgs      = '/s /v/qn EULAS_AGREED=1'
+}
+
+Install-Package $arguments
--- VMWareWorkstation/tools/ChocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$arguments          = @{
-    url             = 'https://download3.vmware.com/software/wkst/file/VMware-workstation-full-12.5.5-5234757.exe'
-    checksum        = 'EF471E8080A7B7B9B2947A8033C32D6F346DC5A21F61022BA49B1DF77BEDDB55'
-    silentArgs      = '/s /nsr /v/qn EULAS_AGREED=1'
-}
-
-Install-Package $arguments
