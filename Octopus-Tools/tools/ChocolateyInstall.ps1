a0541e8e01aad509f6cdff531936555a9c22a945
The Running Dev <therunningdev@gmail.com>
2017-05-07 23:00:06 -0400

    Renamed Octopus-Tools

---
+++ Octopus-Tools/tools/ChocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$arguments      = @{
+    url         = 'https://download.octopusdeploy.com/octopus-tools/4.15.1/OctopusTools.4.15.1.zip'
+    checksum    = 'C49934BD25CD8F6F09095482A5BA14B561BF4F1B378848F303DEB857273A2B0B'
+    destination = Join-Path $env:AppData 'Octopus'
+}
+
+Install-FromZip $arguments
+
+$env:Path = "$($env:Path);$($argumnets.destination)"
--- OctopusTools/tools/ChocolateyInstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$arguments      = @{
-    url         = 'https://download.octopusdeploy.com/octopus-tools/4.14.1/OctopusTools.4.14.1.zip'
-    checksum    = '172F316436C9C2EA920490C369FA2D02B410FCE99C7BC868E9B319017681007E'
-    destination = Join-Path $env:AppData 'Octopus'
-}
-
-Install-FromZip $arguments
-
-$env:Path = "$($env:Path);$($argumnets.destination)"
