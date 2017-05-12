b8f740ffda8bd246e40e7e46a982e3915adde408
The Running Dev <therunningdev@gmail.com>
2017-05-12 07:40:29 -0400

    Added new packages for Microsoft-SQLServer2016Express and Microsoft-SQLServerManagementStudio
    Moved pscx to Powershell-CommunityExtensions

---
+++ Microsoft-SQLServer2016Express/tools/ChocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageChecksum            = '2A5B64AE64A8285C024870EC4643617AC5146894DD59DD560E75CEA787BF9333'
+$defaultConfigurationFile   = Join-Path $env:chocolateyPackageFolder 'Configuration.ini'
+$parameters                 = Get-Parameters $env:chocolateyPackageParameters
+$configurationFile          = Get-ConfigurationFile $parameters['ConfigurationFile'] $defaultConfigurationFile
+$silentArgs                 = "/IAcceptSQLServerLicenseTerms /ConfigurationFile=""$($configurationFile)"""
+
+if (!$parameters.ContainsKey['sqlsysadminaccounts']) {
+    $silentArgs = $silentArgs + " /SQLSYSADMINACCOUNTS=""$(whoami)"""
+}
+
+$installerBase      = 'Microsoft SQL Server 2016 Express'
+$arguments          = @{
+    file            = 'Microsoft SQL Server 2016 Express.exe'
+    silentArgs      = "/Q /x:`"$env:Temp\$installerBase`""
+    executable      = "$env:Temp\$installerBase\Setup.exe"
+    executableArgs  = $silentArgs
+    validExitCodes  = @(2147781575, 2147205120)
+}
+
+Install-Package $arguments
+
+Get-ChildItem $env:Temp -Filter $installerBase | Remove-Item -Recurse -Force
