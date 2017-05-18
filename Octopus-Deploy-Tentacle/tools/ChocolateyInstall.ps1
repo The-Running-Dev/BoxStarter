9674fbfc04b9f4c6302d38daab6e2ef210ae1e1d
The Running Dev <therunningdev@gmail.com>
2017-05-18 09:53:13 -0400

    Added Octopus-Deploy-Tentacle package

---
+++ Octopus-Deploy-Tentacle/tools/ChocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$arguments = @{
+    url      = 'https://download.octopusdeploy.com/octopus/Octopus.Tentacle.3.13.6-x64.msi'
+    checksum = '16ACDF45D5683BCB3A2E2C7B81693DF6A08BFBB75028583F62A31A07B9A29D52'
+}
+
+Install-Package $arguments
