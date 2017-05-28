704f6fce1dc6e059056029ce7b92acd1f480495f
The Running Dev <therunningdev@gmail.com>
2017-05-28 18:14:44 -0400

    Moved VisualCRedistributable packages to Microsoft-

---
+++ Microsoft-VisualCRedistributable2015x64/tools/ChocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$arguments          = @{
+    url             = 'https://download.microsoft.com/download/6/A/A/6AA4EDFF-645B-48C5-81CC-ED5963AEAD48/vc_redist.x64.exe'
+    checksum        = 'DA66717784C192F1004E856BBCF7B3E13B7BF3EA45932C48E4C9B9A50CA80965'
+    silentArgs      = '/Q /norestart'
+}
+
+Install-Package $arguments
