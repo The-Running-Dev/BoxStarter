afe5c279ef78256b555e2602a087fe6aa0c891c6
The Running Dev <therunningdev@gmail.com>
2017-05-29 19:52:43 -0400

    Removed BoxStarter packages from downloads as they are now part of the main directory

--- download/Boxstarter.Bootstrapper/tools/Boxstarter.Bootstrapper/Init-Settings.ps1
+++
@@ -1,6 +0,0 @@
-if(!$Global:Boxstarter) { $Global:Boxstarter = @{} }
-if(!$Boxstarter.ContainsKey('Log')) {
-    $Boxstarter.Log="$(Get-BoxstarterTempDir)\boxstarter.log"
-}
-if(!$Boxstarter.ContainsKey('RebootOk')) { $Boxstarter.RebootOk=$false }
-if(!$Boxstarter.ContainsKey('IsRebooting')) { $Boxstarter.IsRebooting=$false }
--- download/Boxstarter.Chocolatey/tools/Boxstarter.Chocolatey/Init-Settings.ps1
+++
@@ -1,9 +0,0 @@
-$config = Get-BoxstarterConfig
-if(!$BoxStarter.LocalRepo){
-   $BoxStarter.LocalRepo=$config.LocalRepo
-}
-if($BoxStarter.LocalRepo.StartsWith("$env:windir")) {
-   $BoxStarter.LocalRepo = Join-Path $(Get-BoxstarterTempDir) "BuildPackages"
-   if(!(Test-Path $BoxStarter.LocalRepo)) { mkdir $BoxStarter.LocalRepo | Out-Null }
-}
-$Boxstarter.NugetSources=$config.NugetSources
