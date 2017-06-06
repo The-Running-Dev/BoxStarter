0d5c247394854c79ac30464a6357fd51958a6d0a
The Running Dev <therunningdev@gmail.com>
2017-06-06 14:26:49 -0400

    Added RemoteDesktopManager package

---
+++ RemoteDesktopManager/tools/ChocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$arguments = @{
+    url        = 'https://cdn.devolutions.net/download/Setup.RemoteDesktopManager.12.5.6.0.exe'
+    checksum   = '98872A6CA16E066A9B11526CE207F14092115DF6ED0B53F48CA6644E6E9FE5A9'
+}
+
+Install-Package $arguments
---
+++ RemoteDesktopManager/update.ps1
@@ -0,0 +1,27 @@
+param([switch] $force, [switch] $push)
+
+$packageDir = $PSScriptRoot
+
+. (Join-Path $PSScriptRoot '..\Scripts\update.begin.ps1')
+
+function global:au_GetLatest {
+    $releaseUrl = 'https://remotedesktopmanager.com/Home/Download'
+    $versionRegEx = 'Version ([0-9\.]+)'
+    $downloadPageUrl = 'https://remotedesktopmanager.com/Home/ThankYou?f=RDMsetup'
+    $downloadUrlRegEx = 'Setup\.RemoteDesktopManager\..*\.exe'
+
+    $releasePage = Invoke-WebRequest -Uri $releaseUrl -UseBasicParsing
+    $releasePage.Content -match $versionRegEx
+    $version = $matches[1]
+
+    $downloadPage = Invoke-WebRequest -Uri $downloadPageUrl -UseBasicParsing
+    $url = $downloadPage.Links | Where-Object href -match $downloadUrlRegEx | Select-Object -First 1 -ExpandProperty href
+
+    if ($force) {
+        $global:au_Version = $version
+    }
+
+    return @{ Url32 = $url; Version = $version }
+}
+
+. (Join-Path $PSScriptRoot '..\Scripts\update.end.ps1')
