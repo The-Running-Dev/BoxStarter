086f4251ea63c6d480be27f1ad4f81716485e66a
The Running Dev <therunningdev@gmail.com>
2017-06-06 14:18:48 -0400

    Added NotepadPlusPlus package

---
+++ NotepadPlusPlus/tools/ChocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$arguments = @{
+    url                    = 'https://notepad-plus-plus.org/repository/7.x/7.4.1/npp.7.4.1.Installer.x64.exe'
+    checksum               = '7E63B7FB06EC318089193072485E96AC4EDEE5D277E1639D3F1A613885326CBC'
+    silentArgs             = '/S'
+}
+
+Install-Package $arguments
---
+++ NotepadPlusPlus/update.ps1
@@ -0,0 +1,26 @@
+param([switch] $force, [switch] $push)
+
+$packageDir = $PSScriptRoot
+
+. (Join-Path $PSScriptRoot '..\Scripts\update.begin.ps1')
+
+function global:au_GetLatest {
+    $releaseUrl = 'https://notepad-plus-plus.org'
+    $versionRegEx = 'download/v([0-9\.]+).html'
+    $downloadUrlRegEx = 'npp\.([0-9\.]+)\.Installer.x64.exe'
+
+    $releasePage = Invoke-WebRequest -Uri $releaseUrl -UseBasicParsing
+    $downloadPageUrl = $releasePage.links | Where-Object href -match $versionRegEx | Select-Object -First 1 -Expand href
+    $version = $matches[1]
+
+    $downloadPage = Invoke-WebRequest -Uri "$releaseUrl/$downloadPageUrl" -UseBasicParsing
+    $downloadPartialUrl = $downloadPage.links | Where-Object href -match $downloadUrlRegEx | Select-Object -First 1 -Expand href
+
+    if ($force) {
+        $global:au_Version = $version
+    }
+
+    return @{ Url32 = "$releaseUrl$downloadPartialUrl"; Version = $version }
+}
+
+. (Join-Path $PSScriptRoot '..\Scripts\update.end.ps1')
