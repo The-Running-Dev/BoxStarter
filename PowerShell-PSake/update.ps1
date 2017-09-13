9d979100534698c7eb3e0d44eb8ad2eca0dbbcdf
The Running Dev <therunningdev@gmail.com>
2017-09-13 13:36:37 -0400

    Fixed versioning issue with PowerShell-PoshGit, PowerShell-PSake

--- PowerShell-PSake/update.ps1
+++ PowerShell-PSake/update.ps1
@@ -5,14 +5,14 @@ $packageDir = $PSScriptRoot
 . (Join-Path $PSScriptRoot '..\..\BoxStarter-Scripts\update.begin.ps1')
 
 function global:au_GetLatest {
-    $downloadUrl = 'https://github.com/psake/psake/archive/v$version.zip'
+    $url = 'https://github.com/psake/psake/archive/v$version.zip'
     $version = (Get-GitHubVersion 'psake/psake').Version
 
     if ($force) {
-        $global:au_Version = $release.Version
+        $global:au_Version = $version
     }
 
-    return @{ Url32 = $ExecutionContext.InvokeCommand.ExpandString($downloadUrl); Version = $version }
+    return @{ Url32 = $ExecutionContext.InvokeCommand.ExpandString($url); Version = $version }
 }
 
 . (Join-Path $PSScriptRoot '..\..\BoxStarter-Scripts\update.end.ps1')
--- PowerShell-Pester/update.ps1
+++ PowerShell-Pester/update.ps1
@@ -9,7 +9,7 @@ function global:au_GetLatest {
     $version = (Get-GitHubVersion 'pester/Pester').Version
 
     if ($force) {
-        $global:au_Version = $release.Version
+        $global:au_Version = $version
     }
 
     return @{ Url32 = $ExecutionContext.InvokeCommand.ExpandString($downloadUrl); Version = $version }
