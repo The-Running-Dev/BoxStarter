cd910200119fd113a9102d59712612f28971f45f
The Running Dev <therunningdev@gmail.com>
2017-12-06 17:07:03 -0600

    Fixed name casing for jDownloader

--- jDownloader/tools/ChocolateyInstall.ps1
+++ jDownloader/tools/ChocolateyInstall.ps1
@@ -4,7 +4,7 @@ $url64 = 'http://installer.jdownloader.org/ic/JD2SilentSetup_x64.exe'
 
 $arguments = @{
     url        = 'https://download.teamviewer.com/download/TeamViewer_Setup_en.exe'
-    checksum   = 'B348E0701262F54DCB6FA7111973CDE6940102E27691600977A124231FF38E92'
+    checksum   = '4CDCD6B9DF31CD0678F827FE2BAECBCC3C16B3F64997CFDFB91A445782E97A39'
     silentArgs = '-q'
 }
 
