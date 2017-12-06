fd98f21c3e2ec4eaadd6dc4894f9b16d5951b9f1
The Running Dev <therunningdev@gmail.com>
2017-12-06 17:05:40 -0600

    Fixed .NET core runtime and SDK update scripts

--- Microsoft-DotNETCore-Runtime/update.ps1
+++ Microsoft-DotNETCore-Runtime/update.ps1
@@ -6,14 +6,18 @@ $packageDir = $PSScriptRoot
 
 function global:au_GetLatest {
     $releaseUrl = 'https://www.microsoft.com/net/download/core#/runtime'
-    $versionRegEx = '.NET Core ([0-9\.]+) runtime \(Current\)'
-    $downloadLinkRegEx = '\.exe download'
+    $versionRegEx = 'Current \(v([0-9\.]+)\)'
+    $downloadLinkRegEx = 'dotnet-runtime-$version-windows-x64-installer'
+    $thankYouPageUrl = 'https://www.microsoft.com/net/download/thank-you/dotnet-runtime-$version-windows-x64-installer'
 
-    $downloadPage = Invoke-WebRequest -UseBasicParsing -Uri $releaseUrl
-    $downloadLink = $downloadPage.Links | Where-Object outerHTML -match $downloadLinkRegEx | Select-Object -First 1 -Skip 3 -Expand href
+    $releasePage = Invoke-WebRequest -UseBasicParsing -Uri $releaseUrl
+    $version = [regex]::match($releasePage.Content, $versionRegEx).Groups[1].Value
 
-    $url = ((Get-WebURL -Url $downloadLink).ResponseUri).AbsoluteUri
-    $version = [regex]::match($downloadPage.Content, $versionRegEx).Groups[1].Value
+    $downloadLinkRegEx = $ExecutionContext.InvokeCommand.ExpandString($downloadLinkRegEx)
+    $thankYouPageUrl = $ExecutionContext.InvokeCommand.ExpandString($thankYouPageUrl)
+
+    $downloadPage = Invoke-WebRequest -UseBasicParsing -Uri $thankYouPageUrl
+    $url = $downloadPage.Links | Where-Object outerHTML -match '\.exe' | Select-Object -First 1 -Expand href
 
     if ($force) {
         $global:au_Version = $version
--- Microsoft-DotNETCore-SDK/update.ps1
+++ Microsoft-DotNETCore-SDK/update.ps1
@@ -5,15 +5,19 @@ $packageDir = $PSScriptRoot
 . (Join-Path $PSScriptRoot '..\Scripts\update.begin.ps1')
 
 function global:au_GetLatest {
-    $releaseUrl = 'https://www.microsoft.com/net/download/core'
-    $downloadLinkRegEx = '\.exe download'
-    $versionRegEx = 'dotnet\-sdk-([0-9\.]+)\-win-x64\.exe'
+    $releaseUrl = 'https://www.microsoft.com/net/download/core#/runtime'
+    $versionRegEx = 'Current \(v([0-9\.]+)\)'
+    $downloadLinkRegEx = 'dotnet-sdk-$version-windows-x64-installer'
+    $thankYouPageUrl = 'https://www.microsoft.com/net/download/thank-you/dotnet-sdk-$version-windows-x64-installer'
 
-    $downloadPage = Invoke-WebRequest -UseBasicParsing -Uri $releaseUrl
-    $downloadLink = $downloadPage.Links | Where-Object outerHTML -match $downloadLinkRegEx | Select-Object -First 1 -Expand href
+    $releasePage = Invoke-WebRequest -UseBasicParsing -Uri $releaseUrl
+    $version = [regex]::match($releasePage.Content, $versionRegEx).Groups[1].Value
 
-    $url = ((Get-WebURL -Url $downloadLink).ResponseUri).AbsoluteUri
-    $version = [regex]::match($url, $versionRegEx).Groups[1].Value
+    $downloadLinkRegEx = $ExecutionContext.InvokeCommand.ExpandString($downloadLinkRegEx)
+    $thankYouPageUrl = $ExecutionContext.InvokeCommand.ExpandString($thankYouPageUrl)
+
+    $downloadPage = Invoke-WebRequest -UseBasicParsing -Uri $thankYouPageUrl
+    $url = $downloadPage.Links | Where-Object outerHTML -match '\.exe' | Select-Object -First 1 -Expand href
 
     if ($force) {
         $global:au_Version = $version
