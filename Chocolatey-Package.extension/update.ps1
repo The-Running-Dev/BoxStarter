01f0f1534fcc6c682272da1ff238830c5da7a45f
The Running Dev <therunningdev@gmail.com>
2017-12-06 17:09:30 -0600

    Build script now does not depend on external functions
    Fixed path to common update script in Chocolatey packages update scripts

--- Chocolatey-Package.extension/update.ps1
+++ Chocolatey-Package.extension/update.ps1
@@ -2,8 +2,8 @@ param([switch] $force, [switch] $push)
 
 $packageDir = $PSScriptRoot
 
-. (Join-Path $PSScriptRoot '..\..\BoxStarter-Scripts\update.begin.ps1')
+. (Join-Path $PSScriptRoot '..\Scripts\update.begin.ps1')
 
 $isFixedVersion = $false
 
-. (Join-Path $PSScriptRoot '..\..\BoxStarter-Scripts\update.end.ps1')
+. (Join-Path $PSScriptRoot '..\Scripts\update.end.ps1')
--- build.ps1
+++ build.ps1
@@ -4,4 +4,30 @@ param (
     [Parameter(Position = 2)][switch] $force
 )
 
-Invoke-ChocoPack $PSScriptRoot $searchTerm $remote $force
+. .\Scripts\New-ChocoPackage.ps1
+
+$include = '*.zip,*.msi,*.exe'
+$artifacts = '..\..\BoxStarter'
+$vaseSir = $PSScriptRoot
+$searchTerm = $searchTerm -replace '\.\\(.*?)\\', '$1'
+$filter = '*.nuspec'
+
+if (-not $searchTerm) {
+    # Get all packages in the base directory and sub directories
+    $packages = (Get-ChildItem -Path $baseDir -Filter $filter -Recurse)
+}
+else {
+    $packages = @()
+
+    foreach ($p in $searchTerm.split(' ')) {
+        $packages += Get-ChildItem -Path $baseDir -Filter $filter -Recurse | Where-Object { $_.Name -match ".*?$p.*"}
+    }
+}
+
+foreach ($p in $packages) {
+    $currentDir = Split-Path -Parent $p.FullName
+
+    Push-Location $currentDir
+    New-ChocoPackage $p.FullName $artifacts $include $force
+    Pop-Location
+}
