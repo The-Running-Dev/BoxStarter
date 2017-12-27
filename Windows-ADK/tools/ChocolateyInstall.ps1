7f1cc121bb2e2fdc206bcad411cbd38b54eedbce
The Running Dev <therunningdev@gmail.com>
2017-12-27 14:52:36 -0600

    Adding new Windows ADK package

---
+++ Windows-ADK/tools/ChocolateyInstall.ps1
@@ -0,0 +1,10 @@
+﻿$ChocoPackage = @{
+    packageName = 'Windows Assessment and Deployment Kit'
+    url = 'https://go.microsoft.com/fwlink/p/?linkid=859206'
+    checksum = '13965AFA6E6889B5E93CF5222B0E0FEA'
+    silentArgs = "/quiet /norestart /log $env:temp\win_adk.log /features +"
+    fileType = 'exe'
+    validExitCodes = @(0,3010)
+}
+
+Install-ChocolateyPackage @ChocoPackage
---
+++ Windows-ADK/tools/chocolateyuninstall.ps1
@@ -0,0 +1,30 @@
+﻿
+<#
+.SYNOPSIS
+Uninstall windows-adk-all
+
+.DESCRIPTION
+Uninstalls the Windows Assessment and Deployment Kit using the Chocolatey framework
+
+.LINK
+https://msdn.microsoft.com/en-us/windows/hardware/dn913721.aspx
+#>
+
+$Key = @( 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+          'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+          'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' ) |
+   Get-ItemProperty -ErrorAction SilentlyContinue | 
+   where-object DisplayName -match 'Windows Assessment and Deployment Kit'
+
+$Key | Out-String | Write-Verbose
+
+$ChocoPackage = @{
+    SilentArgs = '/uninstall /quiet'
+    packageName = 'Windows Assessment and Deployment Kit'
+    fileType = 'exe'
+    file = $key.BundleCachePath
+    validExitCodes = @(0,3010)
+}
+
+Uninstall-ChocolateyPackage @ChocoPackage
+
